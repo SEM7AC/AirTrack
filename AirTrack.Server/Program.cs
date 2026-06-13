13c49c46f198daa5b4d2da04da4abab6bf8d89bb
Christopher Semler <130243001+SEM7AC@users.noreply.github.com>
2026-06-13 16:52:26 -0700

    Summary: added base project template files
    Details:
    - added _Imports.razor
    - added App.razor
    - added router and route setup
    - added MainLayout.razor
    - added template scaffolding files

---
+++ AirTrack.Server/Program.cs
@@ -0,0 +1,42 @@
+using AirTrack.Server.Components;
+using AirTrack.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirTrack.Server
+    {
+    public class Program
+        {
+        public static void Main(string[] args)
+            {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Register AirTrackContext here
+            builder.Services.AddDbContext<AirTrackContext>(options =>
+            options.UseSqlite("Data Source=airtrack.db"));
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+                {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+                }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+            }
+        }
+    }
