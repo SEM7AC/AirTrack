54f911bd9ae92db8d3b30af6d433be4ad50b1c1c
Christopher Semler <130243001+SEM7AC@users.noreply.github.com>
2026-06-13 21:00:10 -0700

    Summary: Migration Files

---
+++ AirTrack.Server/Migrations/20260614034729_AddUniqueTailNumber.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirTrack.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueTailNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Aircraft_TailNumber",
+                table: "Aircraft",
+                column: "TailNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Aircraft_TailNumber",
+                table: "Aircraft");
+        }
+    }
+}
